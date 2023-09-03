d36ce9850ec591b56af6dc191698324b505a265e
Mnv Kap <Kapoormanav.mk@gmail.com>
2023-09-03 16:36:00 -0400

    Modify file structure

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import sys
 import re
 from PIL import Image
 import xml.etree.ElementTree as ET 
-from find_leafs import find_leaf_nodes
-from draw import draw_highlight
+from utils import draw
+from utils import find_leafs
 
 def main(): 
     image = Image.open(sys.argv[2]) # Load our image 
     tree = ET.parse(sys.argv[1]) # Create ET object 
     root = tree.getroot() # Store root of tree
-    leaf_nodes = find_leaf_nodes(root) # Find all leaf nodes 
+    leaf_nodes = find_leafs.find_leaf_nodes(root) # Find all leaf nodes 
 
     for node in leaf_nodes:
         bounds = node.get("bounds") # Get the value from the attribute bounds
@@ -20,7 +20,7 @@ def main():
         x2, y2 = int( coordinates[2] ), int( coordinates[3] ) 
 
         # Draw on image
-        draw_highlight(image, x1, y1, x2, y2)
+        draw.draw_highlight(image, x1, y1, x2, y2)
 
     image.save("output_image.png") 
     image.show() 
